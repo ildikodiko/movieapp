5f82868b2ee1071e223b2564eef50e012ef8a7fb
your_name <your_email>
2020-03-03 11:12:51 +0100

    Updates

---
+++ precache-manifest.e5e18b7b5e4f4f7c30df539755aac885.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d5a6c29646b1625bd106f452dfef44",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "6b66fbc15e1e8414a393",
+    "url": "/movieapp/static/js/2.1533d5ac.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/movieapp/static/js/2.1533d5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc6f4b7f6bb520ecf2a",
+    "url": "/movieapp/static/js/main.90daccbb.chunk.js"
+  },
+  {
+    "revision": "49f3fbc6c5ec9b43b67c",
+    "url": "/movieapp/static/js/runtime-main.81ad9fd9.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/movieapp/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/movieapp/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/movieapp/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
